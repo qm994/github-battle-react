af732f95add5ef3f22ced0610ada108aad9aa729
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-10-04 12:57:15 -0400

    add the pupular navbar

--- app/index.js
+++ app/index.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Popular from '../app/components/Popular';
 
 // state + lifecycle + ui
 class App extends Component {
     render() {
         return(
-            <div>Hello world!</div>
+            <div className='container'>
+                <Popular />
+            </div>
+            
         )
     }
 }
